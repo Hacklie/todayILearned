2be344b06bb3ca9d74caf34a86ef008654cae0c2
Christian <hacklypselon@gmail.com>
2025-02-13 15:18:53 +0100

    Changed the Supabase Project, because the other one was paused too long

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tsijfzvrxznvzowvglqb.supabase.co";
+const supabaseUrl = "https://rrbjnvtktckumycdshfb.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzaWpmenZyeHpudnpvd3ZnbHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NzE4MzcsImV4cCI6MjAyOTU0NzgzN30.KCbDeKWXTc1mpCTaqunj8MiMGCI8S4zr3SklCp9RJgw";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJyYmpudnRrdGNrdW15Y2RzaGZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk0NTM0MjksImV4cCI6MjA1NTAyOTQyOX0.Ttpp4Arn2jpk8HKdvIZNVu-RZd_rEfff2cVgTPEJBHA";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
